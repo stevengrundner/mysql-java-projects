94fda613e4fe98e2a67139b2e8a360d613a64898
Steven Grundner <133699580+stevengrundner@users.noreply.github.com>
2023-09-14 08:41:17 -0600

    Add files via upload

--- mysql-java-projects/src/main/java/projects/dao/DbConnection.java
+++ mysql-java-projects/src/main/java/projects/dao/DbConnection.java
@@ -17,7 +17,7 @@ public class DbConnection {
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER,
 				PASSWORD);
 
-		System.out.println("Connecting with uri=" + uri);
+	//	System.out.println("Connecting with uri=" + uri);
 
 		try {
 			Connection conn = DriverManager.getConnection(uri);
